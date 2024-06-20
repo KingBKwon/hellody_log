4fd21b3b68d9cd232c2d78195f48001d98f8ba6d
kingbk <wbk6108@naver.com>
2024-06-20 15:14:37 +0900

    오류사항 수정

--- app/routes/watch/fuc.py
+++ app/routes/watch/fuc.py
@@ -4,6 +4,7 @@ input_data = ''
 i = 0
 async def insert_s3(user_id: str, vod_id: int):
     global input_data
+    global i
     start_time = datetime.now()
     user_id = user_id
     vod_id = vod_id
@@ -12,8 +13,9 @@ async def insert_s3(user_id: str, vod_id: int):
     i += 1
     if i > 10:
         await upload_s3(input_data)
-        
-    return input_data
+        i = 0
+        input_data = ''
+        return 's3 업로드'
 
 # Example usage
 # import asyncio
